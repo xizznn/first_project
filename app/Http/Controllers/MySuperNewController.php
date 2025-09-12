af1e9f0b557303ba08a6c90fe1dd5cb426ddf4f9
Kirill <ignatovkirill5@gmail.com>
2025-09-12 18:57:33 +0300

    6. Модель. Метод чтения данных из базы(READ)

---
+++ app/Http/Controllers/MySuperNewController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MySuperNewController extends Controller
+{
+    public function index() {
+        return 'my super new controller';
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use function PHPUnit\Framework\returnArgument;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/my_page', 'MyPlaceController@index');
+
+Route::get('/hello', function() {
+    return 'hello';
+});
+
+Route::get('/my_super_new_controller', 'MySuperNewController@index');
+
+Route::get('/posts', 'PostController@index');
