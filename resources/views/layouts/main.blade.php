2e49f439e7e0d6383bc92d3338fa23f8ac5d0774
Kirill <ignatovkirill5@gmail.com>
2026-01-10 09:21:39 +0300

    34. Класс Policy в Laravel

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,35 +9,42 @@
 
     <title>Document</title>
 </head>
+
 <body>
-<div class="container">
-    <div class="row">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link" href="{{ route('main.index') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="{{ route('about.index') }}">About</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
-                </li>
-            </ul>
-            </div>
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('main.index') }}">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('about.index') }}">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                            </li>
+                            @can('view', auth()->user())
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.post.index') }}">Admin</a>
+                            </li>
+                            @endcan
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
-        </nav>
+        @yield('content')
     </div>
-    @yield('content')
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
+
 </html>
